f16fd8b750bbe7a7de43bc9dd3fc9d7bd7cad94a
Vishesh Gupta <vishesh@stanford.edu>
2017-01-03 15:42:54 -0800

    initial commit, with just the tour of the registry

--- glue/__main__.py
+++
@@ -1,7 +0,0 @@
-import sys
-from glue.codegen import tohtml
-from glue.library import Standard
-from bs4 import BeautifulSoup
-
-if __name__ == '__main__':
-  print(BeautifulSoup(tohtml(Standard, sys.stdin.read()), 'html.parser').prettify())
